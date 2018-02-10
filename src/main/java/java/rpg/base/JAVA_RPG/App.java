2beee33ce31fc9fc33dd7ff85bc3b8f4a4e71f6b
Will Pierlot <willp@outlook.com.au>
2018-02-10 19:03:33 +1100

    initial commit

---
+++ src/main/java/java/rpg/base/JAVA_RPG/App.java
@@ -0,0 +1,13 @@
+package java.rpg.base.JAVA_RPG;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
